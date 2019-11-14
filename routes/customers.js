618ae40a05490bb4026d705f2d27ee8aec02de6d
ronyaryaW1 <bangwijay11@gmail.com>
2019-11-14 09:57:38 +0700

    add login page

--- routes/customers.js
+++ routes/customers.js
@@ -1,86 +1,46 @@
 var express = require('express');
 var router = express.Router();
-
+var authentication_mdl = require('../middlewares/authentication');
+var session_store;
 /* GET Customer page. */
 
-router.get('/', function(req, res, next) {
+router.get('/',authentication_mdl.is_login, function(req, res, next) {
 	req.getConnection(function(err,connection){
 		var query = connection.query('SELECT * FROM customer',function(err,rows)
 		{
 			if(err)
 				var errornya  = ("Error Selecting : %s ",err );   
 			req.flash('msg_error', errornya);   
-			res.render('customer/list',{title:"Customer",data:rows});
+			res.render('customer/list',{title:"Customers",data:rows,session_store:req.session});
 		});
+         //console.log(query.sql);
      });
 });
-router.post('/add', function(req, res, next) {
-	req.assert('name', 'Please fill the name').notEmpty();
-	var errors = req.validationErrors();
-	if (!errors) {
 
-		v_name = req.sanitize( 'name' ).escape().trim(); 
-		v_email = req.sanitize( 'email' ).escape().trim();
-		v_address = req.sanitize( 'address' ).escape().trim();
-		v_phone = req.sanitize( 'phone' ).escape();
-
+router.delete('/delete/(:id)',authentication_mdl.is_login, function(req, res, next) {
+	req.getConnection(function(err,connection){
 		var customer = {
-			name: v_name,
-			address: v_address,
-			email: v_email,
-			phone : v_phone
+			id: req.params.id,
 		}
-
-		var insert_sql = 'INSERT INTO customer SET ?';
+		
+		var delete_sql = 'delete from customer where ?';
 		req.getConnection(function(err,connection){
-			var query = connection.query(insert_sql, customer, function(err, result){
+			var query = connection.query(delete_sql, customer, function(err, result){
 				if(err)
 				{
-					var errors_detail  = ("Error Insert : %s ",err );   
+					var errors_detail  = ("Error Delete : %s ",err);
 					req.flash('msg_error', errors_detail); 
-					res.render('customer/add-customer', 
-					{ 
-						name: req.param('name'), 
-						address: req.param('address'),
-						email: req.param('email'),
-						phone: req.param('phone'),
-					});
-				}else{
-					req.flash('msg_info', 'Create customer success'); 
 					res.redirect('/customers');
-				}		
+				}
+				else{
+					req.flash('msg_info', 'Delete Customer Success'); 
+					res.redirect('/customers');
+				}
 			});
 		});
-	}else{
-		console.log(errors);
-		errors_detail = "Sory there are error<ul>";
-		for (i in errors) 
-		{ 
-			error = errors[i]; 
-			errors_detail += '<li>'+error.msg+'</li>'; 
-		} 
-		errors_detail += "</ul>"; 
-		req.flash('msg_error', errors_detail); 
-		res.render('customer/add-customer', 
-		{ 
-			name: req.param('name'), 
-			address: req.param('address')
-		});
-	}
-
-});
-
-router.get('/add', function(req, res, next) {
-	res.render(	'customer/add-customer', 
-	{ 
-		title: 'Add New Customer',
-		name: '',
-		email: '',
-		phone:'',
-		address:''
 	});
 });
-router.get('/edit/(:id)', function(req,res,next){
+router.get('/edit/(:id)',authentication_mdl.is_login, function(req,res,next){
 	req.getConnection(function(err,connection){
 		var query = connection.query('SELECT * FROM customer where id='+req.params.id,function(err,rows)
 		{
@@ -99,7 +59,7 @@ router.get('/edit/(:id)', function(req,res,next){
 				else
 				{	
 					console.log(rows);
-					res.render('customer/edit',{title:"Edit ",data:rows[0]});
+					res.render('customer/edit',{title:"Edit ",data:rows[0],session_store:req.session});
 
 				}
 			}
@@ -107,7 +67,7 @@ router.get('/edit/(:id)', function(req,res,next){
 		});
 	});
 });
-router.put('/edit/(:id)', function(req,res,next){
+router.put('/edit/(:id)',authentication_mdl.is_login, function(req,res,next){
 	req.assert('name', 'Please fill the name').notEmpty();
 	var errors = req.validationErrors();
 	if (!errors) {
@@ -154,34 +114,79 @@ router.put('/edit/(:id)', function(req,res,next){
 		} 
 		errors_detail += "</ul>"; 
 		req.flash('msg_error', errors_detail); 
+		res.redirect('/customers/edit/'+req.params.id);
+	}
+});
+
+router.post('/add',authentication_mdl.is_login, function(req, res, next) {
+	req.assert('name', 'Please fill the name').notEmpty();
+	var errors = req.validationErrors();
+	if (!errors) {
+
+		v_name = req.sanitize( 'name' ).escape().trim(); 
+		v_email = req.sanitize( 'email' ).escape().trim();
+		v_address = req.sanitize( 'address' ).escape().trim();
+		v_phone = req.sanitize( 'phone' ).escape();
+
+		var customer = {
+			name: v_name,
+			address: v_address,
+			email: v_email,
+			phone : v_phone
+		}
+
+		var insert_sql = 'INSERT INTO customer SET ?';
+		req.getConnection(function(err,connection){
+			var query = connection.query(insert_sql, customer, function(err, result){
+				if(err)
+				{
+					var errors_detail  = ("Error Insert : %s ",err );   
+					req.flash('msg_error', errors_detail); 
+					res.render('customer/add-customer', 
+					{ 
+						name: req.param('name'), 
+						address: req.param('address'),
+						email: req.param('email'),
+						phone: req.param('phone'),
+						session_store:req.session,
+					});
+				}else{
+					req.flash('msg_info', 'Create customer success'); 
+					res.redirect('/customers');
+				}		
+			});
+		});
+	}else{
+
+		console.log(errors);
+		errors_detail = "Sory there are error<ul>";
+		for (i in errors) 
+		{ 
+			error = errors[i]; 
+			errors_detail += '<li>'+error.msg+'</li>'; 
+		} 
+		errors_detail += "</ul>"; 
+		req.flash('msg_error', errors_detail); 
 		res.render('customer/add-customer', 
 		{ 
 			name: req.param('name'), 
-			address: req.param('address')
+			address: req.param('address'),
+			session_store:req.session
 		});
 	}
+
 });
-router.delete('/delete/(:id)', function(req, res, next) {
-	req.getConnection(function(err,connection){
-		var customer = {
-			id: req.params.id,
-		}
-		
-		var delete_sql = 'delete from customer where ?';
-		req.getConnection(function(err,connection){
-			var query = connection.query(delete_sql, customer, function(err, result){
-				if(err)
-				{
-					var errors_detail  = ("Error Delete : %s ",err);
-					req.flash('msg_error', errors_detail); 
-					res.redirect('/customers');
-				}
-				else{
-					req.flash('msg_info', 'Delete Customer Success'); 
-					res.redirect('/customers');
-				}
-			});
-		});
+
+router.get('/add',authentication_mdl.is_login, function(req, res, next) {
+	res.render(	'customer/add-customer', 
+	{ 
+		title: 'Add New Customer',
+		name: '',
+		email: '',
+		phone:'',
+		address:'',
+		session_store:req.session
 	});
 });
+
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,79 @@
 var express = require('express');
 var router = express.Router();
+var modul = require('../modul/modul');
 
+var session_store;
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.redirect('/customers');
 });
-
-router.get('/blankpage', function(req, res, next){
-  res.render('blankpage', { title: 'Express'});
+router.get('/login',function(req,res,next){
+	res.render('main/login',{title:"Login Page"});
 });
+router.post('/login',function(req,res,next){
+	session_store=req.session;
+	req.assert('txtEmail', 'Please fill the Username').notEmpty();
+	req.assert('txtEmail', 'Email not valid').isEmail();
+	req.assert('txtPassword', 'Please fill the Password').notEmpty();
+	var errors = req.validationErrors();
+	if (!errors) {
+		req.getConnection(function(err,connection){
+			v_pass = req.sanitize( 'txtPassword' ).escape().trim(); 
+			v_email = req.sanitize( 'txtEmail' ).escape().trim();
+			
+			var query = connection.query('select * from user where the_email="'+v_email+'" and the_password="'+v_pass+'"',function(err,rows)
+			{
+				if(err)
+				{
 
+					var errornya  = ("Error Selecting : %s ",err.code );  
+					console.log(err.code);
+					req.flash('msg_error', errornya); 
+					res.redirect('/login'); 
+				}else
+				{
+					if(rows.length <=0)
+					{
+
+						req.flash('msg_error', "Wrong email address or password. Try again."); 
+						res.redirect('/login');
+					}
+					else
+					{	
+						session_store.is_login = true;
+						res.redirect('/customers');
+					}
+				}
+
+			});
+		});
+	}
+	else
+	{
+		errors_detail = "Sory there are error<ul>";
+		for (i in errors) 
+		{ 
+			error = errors[i]; 
+			errors_detail += '<li>'+error.msg+'</li>'; 
+		} 
+		errors_detail += "</ul>"; 
+		console.log(errors_detail);
+		req.flash('msg_error', errors_detail); 
+		res.redirect('/login'); 
+	}
+});
+router.get('/logout', function(req, res)
+{ 
+	req.session.destroy(function(err)
+	{ 
+		if(err)
+		{ 
+			console.log(err); 
+		} 
+		else 
+		{ 
+			res.redirect('/login'); 
+		} 
+	}); 
+});
 module.exports = router;
